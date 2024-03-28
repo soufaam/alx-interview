244e3ef201fe58602a6c43136c75443480689081
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-03-28 23:53:21 +0000

    0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Write a script that reads stdin line by line and computes metrics:"""
+import sys
+import re
+
+
+def check_format(input_string):
+    """ check correct format"""
+    pattern = (
+        r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+        r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] '
+        r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+    )
+    match = re.match(pattern, input_string)
+    if match:
+        return True
+    else:
+        return False
+
+
+status_list = [200, 301, 400, 401, 403, 404, 405, 500]
+count = 0
+status_dic = {}
+file_size = 0
+line = sys.stdin.readline()
+while line != '':
+    if not check_format(line):
+        continue
+    print(line)
+    if count == 10:
+        count = 0
+        print(f'File size: {file_size}')
+        for scode in status_list:
+            if scode in status_dic:
+                print(f'{scode}: {status_dic[scode]}')
+        file_size = 0
+    splited_line = line.split()
+    status_code = int(splited_line[-2])
+    if status_code in status_list:
+        if status_code in status_dic:
+            status_dic[status_code] += 1
+        else:
+            status_dic[status_code] = 1
+    count += 1
+    file_size += int(splited_line[-2])
+    line = sys.stdin.readline()
