f6c0a5c743388b45a4b0913f426663b787f144d3
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-04 22:55:53 +0000

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write a method that determines if a given data set
+represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """Return: True if data is a valid UTF-8 encoding,
+    else return False"""
+    for element in data:
+        if 0 < element < 256:
+            continue
+        else:
+            return False
+    return True
