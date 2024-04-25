3780c9c813cfe83722b4decb83c171e30c0c6baa
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-26 00:31:19 +0100

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Rotate 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
