9b6d0a3f1b8e70e71ae6243f34c00fc4e664649e
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-05-09 16:06:47 +0100

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """a function island_perimeter to calculate
+    the perimeter of the island """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+    return perimeter
